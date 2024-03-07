a7f38a498b435f06d365816f03bafc10eae45964
Sean West <sean.west@flatironschool.com>
2024-03-06 23:00:19 -0500

    lubuntu desktop 22

---
+++ lubuntu22/create_and_export.ps1
@@ -0,0 +1,30 @@
+vagrant destroy -f
+vagrant up
+vagrant ssh -c 'sudo apt update -yq && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -yq'
+vagrant halt
+
+VBoxManage sharedfolder remove "lubuntu22" --name "vagrant"
+
+VBoxManage startvm lubuntu22 --type headless
+echo "Waiting 120 seconds for VM to fully boot back up..."
+sleep 40
+echo "80 more seconds..."
+sleep 40
+echo "40 more seconds..."
+sleep 40
+
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22004 -i ../keys/id_rsa root@localhost 'bash ~vagrant/teardown.sh'
+echo "Waiting 20 seconds before shutting down VM..."
+sleep 20
+
+VBoxManage controlvm lubuntu22 poweroff
+sleep 5
+
+VBoxManage modifyvm "lubuntu22" --natpf1 delete "ssh"
+VBoxManage modifyvm "lubuntu22" --natpf1 delete "guestssh"
+
+echo "Exporting to .ova. This may take a while."
+rm -f "../ovas/lubuntu22.ova"
+VBoxManage export lubuntu22 --output "../ovas/lubuntu22.ova" --options nomacs
+
+echo "Done!"
---
+++ lubuntu22/files/teardown.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf /root/.ssh
+
+#deluser --remove-home ubuntu
+#rm /etc/sudoers.d/90-cloud-init-users
+
+rm /etc/sudoers.d/vagrant
+deluser --remove-home vagrant
+rm /vagrant
